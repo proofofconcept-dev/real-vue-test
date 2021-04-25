fcf5c2bf80a76856b5107fbbff8be457bbce52a7
Vue user <proofofconcept2018@gmail.com>
2021-04-25 10:39:44 -0600

    Vuex and progress bar added using Nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,32 +3,30 @@ export default {
   head: {
     title: 'real-world-nuxt',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
+  loading: {
+    color: '#39b982',
+    height: '5px',
+  },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -40,6 +38,5 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
---
+++ pages/event/create.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+<h1>Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'You can create a new event taking place in your neighborhood',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style  scoped>
+</style>
